13a27435d738f1ec5dc452d11a94a6f3773da5a1
jung seung hoon <todtkddj@naver.com>
2022-03-04 11:15:13 +0900

    nest 3일차

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+  Header,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -17,8 +27,12 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @Header('Custom', 'Test Header')
   @Get(':id')
   findOne(@Param('id') id: string) {
+    if (+id < 1) {
+      throw new BadRequestException('id는 0보다 큰 값이어야 합니다.');
+    }
     return this.usersService.findOne(+id);
   }
 
