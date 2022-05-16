2ffd3e531fa6342b609f77ee4effd763d7de6a1a
jung seung hoon <todtkddj@naver.com>
2022-05-16 19:03:32 +0900

    프로바이더 준비

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
   imports: [],
   controllers: [UsersController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
